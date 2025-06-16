29fe9116e1a8b84246400a575fe6abf9128adf35
tristinxie <tristinxie@gmail.com>
2025-06-16 01:42:15 -0700

    Record button press history

--- backend/app.py
+++ backend/app.py
@@ -4,14 +4,25 @@ import threading
 from gpiozero import LED, Device
 from gpiozero.pins.mock import MockFactory
 from waitress import serve
+import sqlite3 as sql
+import os
+from datetime import datetime
+import json
 
 Device.pin_factory = MockFactory()
-
+DATABASE_PATH = "metadata.db"
 led = LED(17)
 app = Flask(__name__)
 CORS(app)
 def failsafe_off():
     led.off()
+@app.route("/metadata", methods=['GET'])
+def get_metadata():
+    with sql.connect(DATABASE_PATH) as con:
+        cur = con.cursor()
+        cur.execute("SELECT * FROM button_history")
+        res = cur.fetchall()[::-1]
+        return json.dumps(dict(res))
 
 @app.route("/button_state", methods=['GET'])
 def get_button_state():
@@ -20,6 +31,10 @@ def get_button_state():
 @app.route("/activate_button", methods=['POST'])
 def activate_button():
     led.on()
+    with sql.connect(DATABASE_PATH) as con:
+        cur = con.cursor()
+        cur.execute(f"INSERT INTO button_history VALUES('{datetime.now()}', 'TX')")
+        con.commit()
     timer = threading.Timer(20.0, failsafe_off)
     timer.start()
     return jsonify({"message": "Button activated"})
@@ -31,4 +46,9 @@ def release_button():
 
 if __name__ =="__main__":
     # app.run()
+    if not os.path.exists(DATABASE_PATH):
+        with sql.connect(DATABASE_PATH) as con:
+            cur = con.cursor()
+            cur.execute("CREATE TABLE button_history(name, press_time)")
+            con.commit()
     serve(app, port=5001, host="0.0.0.0")
--- mpm-frontend/src/App.jsx
+++ mpm-frontend/src/App.jsx
@@ -4,16 +4,32 @@ import RemoteButton from "./RemoteButton"
 import StatusLight from './StatusLight';
 function App() {
   const [buttonDown, setButtonDown] = useState(null);
-  const [loading, setLoading] = useState(true)
+  const [loading, setLoading] = useState(true);
+  const [metadata, setMetadata] = useState([])
   useEffect(() => {
     fetchButton()
   }, [])
+  useEffect(() => {
+    fetchMetadata()
+  }, [buttonDown])
   const fetchButton = async () => {
 		const url = "http://" + import.meta.env.VITE_SERVER_IP + ":" + import.meta.env.VITE_SERVER_PORT + "/button_state";
     const res = await fetch(url);
     const data = await res.json();
     setButtonDown(data.state)
     setLoading(false)
+  }
+
+  const fetchMetadata = async () => {
+		const url = "http://" + import.meta.env.VITE_SERVER_IP + ":" + import.meta.env.VITE_SERVER_PORT + "/metadata";
+    const res = await fetch(url);
+    let data = await res.json();
+    data = Object.entries(data)
+    data.map(d => {
+      const date = new Date(d[0])
+      d[0] = date
+    })
+    setMetadata(data)
   }
 
   if (loading) {
@@ -26,6 +42,13 @@ function App() {
         <RemoteButton buttonDown={buttonDown} setButtonDown={setButtonDown} />
         <hr style={{'borderTop': '2px solid #2a2a2a', 'marginTop': '80px', 'height': '2px'}} />
         <h2 style={{'fontFamily': 'sans-serif', 'color': '#3b3b3b'}}>MULTI &middot; PI &middot; MOTE</h2>
+      </div>
+      <div>
+        {metadata.map(data => (
+          <div key={data[0].getTime()}>
+            <p>{data[1]} pressed at {data[0].toLocaleTimeString()} {data[0].toLocaleDateString()}</p>
+          </div>
+        ))}
       </div>
     </>
   )
