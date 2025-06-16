a371ae61eaf5f008022d3159e34eac7d85435d43
Tristin Xie <tristinxie@gmail.com>
2025-06-16 01:38:18 -0700

    Working basic server

--- backend/app.py
+++ backend/app.py
@@ -2,29 +2,28 @@ from flask import Flask, request, jsonify
 from flask_cors import CORS
 import threading
 from gpiozero import LED, Device
-from gpiozero.pins.mock import MockFactory
 from waitress import serve
+from gpiozero.pins.mock import MockFactory
 
-Device.pin_factory = MockFactory()
-
+# Device.pin_factory = MockFactory()
 led = LED(17)
 app = Flask(__name__)
 CORS(app)
 def failsafe_off():
     led.off()
 
-@app.route("/button_state", methods=['GET'])
+@app.route("/api/button_state", methods=['GET'])
 def get_button_state():
     return jsonify({"state": led.is_lit})
 
-@app.route("/activate_button", methods=['POST'])
+@app.route("/api/activate_button", methods=['POST'])
 def activate_button():
     led.on()
     timer = threading.Timer(20.0, failsafe_off)
     timer.start()
     return jsonify({"message": "Button activated"})
 
-@app.route("/release_button", methods=['POST'])
+@app.route("/api/release_button", methods=['POST'])
 def release_button():
     led.off()
     return jsonify({"message": "Button released"})
